5a07fe00efd764f0d38647d95d68b38cd62962c4
Sunil Suman <99410256+sunilsuman81@users.noreply.github.com>
2024-12-07 15:15:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,29 @@
-number = [1,2,3]
-new_number =[ n +1 for n in number]
-print(new_number)
+names = ["sunil", "shanaya", "Shantanu", "Suman", "SKSUMAN"]
 
-name = "sunil"
-letter_list = [letter for letter in name]
-print(letter_list)
+import random
 
-number_list = [number * 2  for number in  range(1, 5)]
-print(number_list)
+student_score = {student: random.randint(1, 100) for student in names}
+print(student_score)
+
+passed_student = {student:score for (student, score) in student_score.items() if score > 60}
+print(passed_student)
+
+#Dictionary Comprehension 1
+sentence = "What is the Airspeed Velocity of an Unladen Swallow?"
+split_word = sentence.split()
+words = {word for word in split_word}
+print(words)
+result = {w : len(w) for w  in words}
+print(result)
+print(result.items())
+cn = {e for (c,e) in result.items()}
+print(cn)
+#Dictionary Comprehension 2
+
+weather_c = {"Monday": 12, "Tuesday": 14, "Wednesday": 15, "Thursday": 14, "Friday": 21, "Saturday": 22, "Sunday": 24}
+temp_c = {day:(temp * 9/5)+32 for (day , temp) in weather_c.items()}
+print(temp_c)
 
 
-name_list = ["sunil", "shanaya", "shantanu", "rupam", "ishu", "lisa"]
-names = [n for n in name_list if len(n) < 5]
-print(names)
-
-names_list = [n.upper() for n in name_list if len(n) > 5]
-print(names_list)
 
 
